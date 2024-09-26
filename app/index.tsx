e71487e6a3556882a967bb8ecd2b13607516ce17
unknown <nodirbekrejabaliyevps@gmail.com>
2024-09-27 04:53:14 +0500

    sefa

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
 
 const index = () => {
   return (
     <View>
-        
       <Text>index</Text>
     </View>
   )
 }
 
 export default index
-
-const styles = StyleSheet.create({})
